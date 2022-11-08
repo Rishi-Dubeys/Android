22fe76557a0452104c3160e8fb8e9d93fa6268b1
dacost13 <dacost13@uni.coventry.ac.uk>
2022-11-08 12:42:04 +0000

    Adding Login and Register xml files // Bottom Navigation View

--- app/src/main/java/com/example/noidea/MainActivity.java
+++ app/src/main/java/com/example/noidea/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.noidea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = (Button) findViewById(R.id.loginHome);
+        Button register = (Button) findViewById(R.id.registerHome);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+            }
+        });
 
     }
 }
