26e57010cbf7d1dd79f4b200569e8682e1362eed
dacost13 <dacost13@uni.coventry.ac.uk>
2022-11-16 22:07:30 +0000

    Create Model and View for home Fragment

---
+++ app/src/main/java/com/example/noidea/model/newGames.java
@@ -0,0 +1,34 @@
+package com.example.noidea.model;
+
+public class newGames {
+
+    String name , platform , releaseDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public newGames() {
+    }
+
+}
---
+++ app/src/main/java/com/example/noidea/viewModel/newsGamesView.java
@@ -0,0 +1,4 @@
+package com.example.noidea.viewModel;
+
+public class newsGamesView {
+}
