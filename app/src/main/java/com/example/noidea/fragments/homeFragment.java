0d5c12ac2157b12a950151f9efb46301d0f4896d
dacost13 <dacost13@uni.coventry.ac.uk>
2022-11-23 17:48:15 +0000

    Add Search Bar in Home fragment_home.xml

--- app/src/main/java/com/example/noidea/fragments/homeFragment.java
+++ app/src/main/java/com/example/noidea/fragments/homeFragment.java
@@ -4,14 +4,19 @@ import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.SearchView;
 
 import com.example.noidea.R;
 import com.example.noidea.model.newGames;
@@ -20,6 +25,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -81,6 +87,30 @@ public class homeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        EditText searchbar = view.findViewById(R.id.search_bar);
+        searchbar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!editable.toString().isEmpty()){
+                    search(editable.toString());
+                }
+                else {
+                    search("");
+                }
+
+            }
+        });
+
         // Assign recycler view with a linear layout
         recyclerView = view.findViewById(R.id.recyclerHome);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -118,4 +148,39 @@ public class homeFragment extends Fragment {
         };
         dbRef.addValueEventListener(postListener);
     }
+
+
+    public void search(String s){
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("News");
+
+        Query query = dbRef.orderByChild("name").startAt(s).endAt(s + "u\uf8ff");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.hasChildren()){
+                    newGamesList.clear();
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                        final newGames newGames = dataSnapshot.getValue(newGames.class);
+                        newGamesList.add(newGames);
+                    }
+                }
+                newsGamesView adapter = new newsGamesView(getContext(),newGamesList);
+                // setting the adapter to the recycler view
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+
+
+
 }
